9c68d284536f766842b75c1e60ce237cdb59b03a
MHMasoon <mhmasoon2000@gmail.com>
2024-12-10 03:12:48 +0330

    add autopairing for some characters

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,1 +1,8 @@
 vim.keymap.set('n', '<leader>e', ':Explore<CR>')
+
+-- Enable auto pairing
+vim.keymap.set('i', '{', '{}<Left>', { noremap = true })
+vim.keymap.set('i', '(', '()<Left>', { noremap = true })
+vim.keymap.set('i', '[', '[]<Left>', { noremap = true })
+vim.keymap.set('i', '"', '""<Left>', { noremap = true })
+vim.keymap.set('i', "'", "''<Left>", { noremap = true })
