6ef2bd79a397ac17a146d677dd8f0f25e638ae3a
MHMasoon <mhmasoon2000@gmail.com>
2025-01-31 17:17:00 +0330

    change comment-nvim configs to default

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -3,15 +3,15 @@
 
 return {
   'numToStr/Comment.nvim',
-  -- opts = {
-  --   ignore = '^$',
-  --   toggler = {
-  --     line = '<leader>cc',
-  --     block = '<leader>bc',
-  --   },
-  --   opleader = {
-  --     line = '<leader>c',
-  --     block = '<leader>b',
-  --   },
-  -- },
+  opt = {
+    padding = true,
+    sticky = true,
+    ignore = nil,
+    toggler = { line = 'gcc', block = 'gbc' },
+    opleader = { line = 'gc', block = 'gb' },
+    extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+    mappings = { basic = true, extra = true },
+    pre_hook = nil,
+    post_hook = nil,
+  },
 }
